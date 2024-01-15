2a1634d9efd54d5d250f01dec185a691a1d7d3f4
PabloNahra <pnahra@satrendy.com>
2024-01-14 23:33:09 -0300

    Conflicto para llamar get de productos desde views

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { productsModel } from "../models/products.model.js"
-import productsRoutes from "./productsFS.routes";
+import productsRoutes from "./products.routes.js";
+
 
 const viewsRoutes = Router()
 
@@ -30,8 +30,30 @@ viewsRoutes.get('/products', async (req, res) => {
     }
 })
 */
+
+
 viewsRoutes.get('/products', async (req, res) => {
-    res.render('products', {products: products.docs})
+    //res.render('products', {products: [{title: "hola"}]})
+    //const productos = {products: [{title: "hola"}]}
+    //console.log(productos)
+    //res.render('products', {products: productos})
+    try {
+      console.log("Entre al try")
+      // Utiliza directamente el método get de productsRoutes
+      const {category, page } = req.query;
+      const products = await productsRoutes.get(category, page);
+      console.log("products")
+      console.log(products)
+      // Renderiza la vista 'products' con el objeto products como contexto
+      res.render('products', { products });
+    } catch (error) {
+      // Manejar errores aquí
+      console.error(error);
+      res.status(500).send('Error interno del servidor');
+    }
 })
+
+
+
 
 export default viewsRoutes

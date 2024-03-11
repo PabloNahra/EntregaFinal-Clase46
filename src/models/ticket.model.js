ce63c9a89e479bcbebf86a99d0570db4a8c37e7e
PabloNahra <pnahra@satrendy.com>
2024-03-10 23:17:41 -0300

    Modelo de tickets agregado

--- src/models/ticket.model.js
+++ src/models/ticket.model.js
@@ -1,38 +1,40 @@
 import mongoose from "mongoose";
 import mongoosePaginate from 'mongoose-paginate-v2'
+import crypto from 'crypto'
 
 const ticketsCollection = 'tickets'
 
 const ticketsSchema = mongoose.Schema({
     code: {
         type: String,
+        unique: true,
         required: true,
-        unique: true
+        default: generateRandomCode // Usamos una función para generar el código aleatorio por defecto
     },
-    price: {
-        type: Number,
-        required: false
+    purchase_datetime: {
+        type: Date,
+        required: true
     },
-    available: {
-        type: Boolean,
-        required: true,
-        default: 1
-    },
-    stock: {
+    amount:{
         type: Number,
         required: true,
         default: 0
     },
-    category: {
-        type: String,
-        enum: ['casa','bebe'],
-        required: true,
-    },
-    thumbnail: {
-        type: [],
-        required: false
-    } 
+    purcharser: {
+       type: String,
+       required: true
+    }   
 })
+
+// Función para generar un código aleatorio de 15 caracteres alfanuméricos
+function generateRandomCode() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let code = '';
+    for (let i = 0; i < 15; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
 
 ticketsSchema.plugin(mongoosePaginate)
 

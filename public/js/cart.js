93de5ef80dc5cdcabe2dfad351a835b8e5d97f6f
PabloNahra <pnahra@satrendy.com>
2024-05-17 20:30:08 -0300

    Solucionado el problema de que handlebars no cargaba cart.js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,9 @@
 // cart.js
 
 document.addEventListener("DOMContentLoaded", function () {
+
+  console.log("DOM completamente cargado y parseado");
+  
   // Obtener todos los botones de aumento y disminución
   const increaseButtons = document.querySelectorAll(".increase-quantity-btn");
   const decreaseButtons = document.querySelectorAll(".decrease-quantity-btn");
@@ -28,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
   decreaseButtons.forEach(function (button) {
     button.addEventListener("click", function () {
       // Obtener el id del producto del botón
+      console.log("click en decrease")
       const productId = button.getAttribute("data-product-id");
       // Obtener el input de cantidad asociado a este producto
       const quantityInput = document.getElementById("quantity" + productId);
@@ -36,47 +40,48 @@ document.addEventListener("DOMContentLoaded", function () {
       // Restar la cantidad en 1 si la cantidad actual es mayor que 1
       if (quantity > 1) {
         quantity--;
+        // Actualizar el valor del input de cantidad
+        quantityInput.value = quantity;
+
+
+        // Actualización al servidor
+        updateCart(productId, quantity);
       }
-      // Actualizar el valor del input de cantidad
-      quantityInput.value = quantity;
     });
   });
 });
 
-
-
 function updateCart(productId, newQuantity) {
+  console.log("Dentro de updateCart");
+  console.log(productId);
+  console.log(newQuantity);
 
-    console.log("Dentro de updateCart")
-    console.log(productId)
-    console.log(newQuantity)
+  // Construir el body de la solicitud
+  const body = JSON.stringify({
+    products: [
+      { 
+        product: productId,
+        quantity: newQuantity
+      }
+    ]
+  });
 
-    // Construir el body de la solicitud
-    const body = JSON.stringify({
-        products: [
-            { 
-                product: productId,
-                quantity: newQuantity
-            }
-        ]
-    });
-
-    // Configurar la solicitud fetch
-    fetch("http://localhost:8080/api/carts/65e8d8ace25d0afffb59ce1a", {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: body
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Error al actualizar el carrito");
-        }
-        // Manejar la respuesta si es necesario
-        console.log("El carrito se ha actualizado correctamente");
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
+  // Configurar la solicitud fetch
+  fetch("http://localhost:8080/api/carts/65e8d8ace25d0afffb59ce1a", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: body
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Error al actualizar el carrito");
+    }
+    // Manejar la respuesta si es necesario
+    console.log("El carrito se ha actualizado correctamente");
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
 }
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -178,6 +178,7 @@ viewsRoutes.get("/cart", checkAuth, async (req, res) => {
 });
 */
 
+/*
 // Vista del carrito según cId
 viewsRoutes.get("/cart/:cId", checkAuth, async (req, res) => {
   try {
@@ -207,6 +208,77 @@ viewsRoutes.get("/cart/:cId", checkAuth, async (req, res) => {
       // Chequear que el carrito al que entramos es del usuario
 
 
+      // Tomar el carrito más reciente si hay alguno después de filtrar y ordenar
+      if (filteredCarts.length > 0) {
+        ultCart = filteredCarts[0];
+
+        // Buscar los detalles de los productos asociados al carrito
+        const productsDetails = await Promise.all(
+          ultCart.products.map(async (productItem) => {
+            const product = await productsModel.findById(productItem.product);
+            return {
+              product: {
+                _id: product._id,
+                title: product.title,
+                price: product.price,
+              },
+              quantity: productItem.quantity,
+            };
+          })
+        );
+
+        // Crear un nuevo array de productos con los detalles actualizados
+        const updatedProducts = ultCart.products.map((productItem, index) => {
+          return {
+            product: productsDetails[index].product,
+            quantity: productItem.quantity,
+            _id: productItem._id,
+          };
+        });
+
+        // Asignar el nuevo array de productos a ultCart.products
+        ultCart.productsDetails = updatedProducts;
+      } else {
+        ultCart = null; // Si no hay carritos después de filtrar y ordenar
+      }
+    }
+
+    // Renderizar la vista cart.handlebars con los datos del usuario y del carrito
+    res.render("cart", { user, cart: ultCart, cId });
+  } catch (error) {
+    // Manejar errores
+    console.error("Error:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+*/
+
+viewsRoutes.get("/cart/:cId", checkAuth, async (req, res) => {
+  try {
+    const { user } = req.session;
+    const { cId } = req.params;
+    let ultCart;
+
+    // Obtener el correo electrónico del usuario
+    const uEmail = user.email;
+
+    // Obtener los carritos del usuario por su correo electrónico
+    const userCarts = await cartManager.getCartByUserEmail(uEmail);
+
+    // Verificar si se obtuvieron correctamente los carritos del usuario
+    if (userCarts.message === "OK") {
+      const carritos = userCarts.rdo;
+
+      // Filtrar los carritos según los estados y el _id  que llega por parametros
+      const filteredCarts = carritos.filter((cart) => {
+        const status = cart.status ? cart.status.toUpperCase() : null;
+        return (
+          status !== "FINALIZADO" &&
+          status !== "CANCELADO" &&
+          cart._id.toString() === cId
+        );
+      });
+   
       // Tomar el carrito más reciente si hay alguno después de filtrar y ordenar
       if (filteredCarts.length > 0) {
         ultCart = filteredCarts[0];
